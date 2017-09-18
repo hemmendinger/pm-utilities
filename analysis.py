fff3d33fcf07070206ad088bba7d36f584bf31a4
Hemmendinger <github@hemmendinger.net>
2017-09-18 11:57:43 -0400

    began creating functionality to analyze and score forecasts

---
+++ analysis.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+def calculate_daily_brier_score(forecasts, answer_keys):
+    '''
+
+    :param forecasts:
+    :param answer_keys: Dictionary with answer keys as keys and True or False as the values
+    :return:
+    '''
+
+    # timestamp formatted as  '2017-09-03T16:31:49Z'
+    prev_dt = datetime.strptime(forecasts[0]['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
+
+    for fc in forecasts:
+        # see if date is equal to same day as prev, if
+        pass
+
+
+def score_forecast(forecast, answer_keys):
+    # Converts string percent to decimal
+    pct = lambda x: int(x.strip('%')) / 100
+
+    score = 0
+
+    for k in answer_keys.keys():
+        if answer_keys[k] is False:
+            score += (0 - pct(forecast[k]))**2
+        else:
+            score += (1 - pct(forecast[k]))**2
+
+    return score
